6cd6e6e7e213fbca8797d6087affe2ee3f757e51
Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>
2024-10-24 13:32:19 -0300

    inital commit
    Signed-off-by: Octavio Piratininga <59701790+Pira4Ever@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,20 @@
+let nome = "Pira4Ever";
+let nivel = 8900;
+
+if (nivel <= 1000) {
+  console.log(`O Herói de nome ${nome} está no nível de Ferro`);
+} else if (nivel <= 2000) {
+  console.log(`O Herói de nome ${nome} está no nível de Bronze`);
+} else if (nivel <= 5000) {
+  console.log(`O Herói de nome ${nome} está no nível de Prata`);
+} else if (nivel <= 7000) {
+  console.log(`O Herói de nome ${nome} está no nível de Ouro`);
+} else if (nivel <= 8000) {
+  console.log(`O Herói de nome ${nome} está no nível de Platina`);
+} else if (nivel <= 9000) {
+  console.log(`O Herói de nome ${nome} está no nível de Ascendente`);
+} else if (nivel <= 10000) {
+  console.log(`O Herói de nome ${nome} está no nível de Imortal`);
+} else {
+  console.log(`O Herói de nome ${nome} está no nível de Radiante`);
+} 
